6c7cf4cd2ac4b9daee8a9e1625e501d40071867c
Khandakar Jarin Shova <jarinshova007@gmail.com>
2025-09-07 22:49:06 +0600

    feat: add Pattern16.java program

---
+++ 04_patterns/Pattern16.java
@@ -0,0 +1,29 @@
+// print the hollow Rhombus
+//      *****
+//     *   *
+//    *   *
+//   *   *
+//  *****
+
+import java.util.Scanner;
+
+public class Pattern16 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("row: ");
+    int row = sc.nextInt();
+    for (int i = 1; i <= row; i++) {
+        for (int j = 1; j <= row - i; j++) {
+            System.out.print(" ");
+        }
+        for (int j = 1; j <= row; j++) {
+            if (i == 1 || i == row || j == 1 || j == row) {
+                System.out.print("*");
+            } else {
+                System.out.print(" ");
+            } 
+        }
+        System.out.println();
+    }
+    }
+}
